f3938aa68f57d003c60aaeff98684e87ddfd399f
Nima Yousefi <inbox@nimayousefi.com>
2013-09-18 19:27:40 -0700

    Add Sample Project for iOS

---
+++ Sample Project iOS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Sample Project iOS
+//
+//  Created by Nima Yousefi on 9/18/13.
+//  Copyright (c) 2013 Nima Yousefi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Sample Project iOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sample Project iOS
+//
+//  Created by Nima Yousefi on 9/18/13.
+//  Copyright (c) 2013 Nima Yousefi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
