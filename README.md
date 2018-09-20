962ca8741590d49611d819f595445bba67054378
Tom Stovall <stovak@gmail.com>
2018-09-19 19:55:54 -0700

    Compiles with Xcode 10

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fountain
+
+A description of this package.
---
+++ Sources/Fountain/FNPaginator.h
@@ -0,0 +1,54 @@
+//
+//  FNPaginator.h
+//
+//  Copyright (c) 2012-2013 Nima Yousefi & John August
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy 
+//  of this software and associated documentation files (the "Software"), to 
+//  deal in the Software without restriction, including without limitation the 
+//  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+//  sell copies of the Software, and to permit persons to whom the Software is 
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in 
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+//  IN THE SOFTWARE.
+//
+#if TARGET_OS_IPHONE
+#import <UIKit/UIKit.h>
+#else
+#import <AppKit/AppKit.h>
+#endif
+#import <Foundation/Foundation.h>
+
+#if TARGET_OS_IPHONE
+    #define QUQFont UIFont
+#else
+    #define QUQFont NSFont
+#endif
+
+@class FNScript, FNElement;
+
+@interface FNPaginator : NSObject
+
+@property (nonatomic, readonly) NSUInteger numberOfPages;
+
+- (id)initWithScript:(FNScript *)aScript;
+- (void)paginate;
+- (void)paginateForSize:(CGSize)pageSize;
+- (NSArray *)pageAtIndex:(NSUInteger)index;
+
+// Helper methods
++ (NSInteger)spaceBeforeForElement:(FNElement *)element;
++ (NSInteger)leftMarginForElement:(FNElement *)element;
++ (NSInteger)widthForElement:(FNElement *)element;
++ (NSInteger)heightForString:(NSString *)string font:(QUQFont *)font maxWidth:(NSInteger)maxWidth lineHeight:(NSInteger)lineHeight;
+
+@end
